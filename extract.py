e3ece8d07adf81dcaf8d55d306a2d97d29f163b1
babooloon <paulyang234@gmail.com>
2018-11-01 18:51:15 -0700

    Finish extract script

--- extract.py
+++ extract.py
@@ -1,4 +1,8 @@
 import pyshark
+import xml.etree.ElementTree as ET
+import hashlib
+import sys
+from M2Crypto import SMIME, BIO
 
 def extract(rtps):
     lfs = rtps.get('rtps.property_name').fields
@@ -7,13 +11,16 @@ def extract(rtps):
         if lf.show == 'c.perm':
             idx = i
             break
-    udBin = rtps.get('rtps.param.userData').fields[idx].show
-    ud = bytes.fromhex(''.join(udBin.split(':'))).decode('utf-8')
-    
-    #TODO: Extract xml data from userdata and generate a xml permission file with 256 hash of corresponding subject_name.
+    ud = bytes.fromhex(rtps.get('rtps.param.userData').fields[idx].show.replace(':', ''))
+    bio = BIO.MemoryBuffer(ud)
+    p7, data = SMIME.smime_load_pkcs7_bio(bio)
+    xml_str = data.read().decode('utf-8').split('\n',2)[2]
+    filename = hashlib.sha256(ud).hexdigest()
+    with open(filename+'.xml', 'w') as f:
+        f.write(xml_str)
 
 if __name__ == '__main__':
-    cap = pyshark.FileCapture('tshark-recording.pcapng', display_filter='rtps.property_name == "c.perm"')
+    cap = pyshark.FileCapture(sys.argv[1], display_filter='rtps.property_name == "c.perm"')
 
     for pac in cap:
         extract(pac.rtps)
