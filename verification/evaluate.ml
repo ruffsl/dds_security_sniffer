d68bd5d9c0ce8ee417c3e4a0f14dc1eff58288a4
Ruffin <roxfoxpox@gmail.com>
2018-11-06 20:12:59 -0800

    Updating evaluate
    to use structs

--- verification/evaluate.ml
+++ verification/evaluate.ml
@@ -1,62 +1,47 @@
-let checkCriteria (cri : criteria) (sub : subject) : bool = true
+let checkCriteria (cri : criteria list) (sub : subject) : bool = true
 
-let rec match_domain (domains : domain nonEmptyList) (id : int) : bool = 
-  let nodeAndNext = 
-    (match domains with 
+let rec match_domain (domains : domain nonEmptyList) (id : int) : bool =
+  let nodeAndNext =
+    (match domains with
     | Node(d) -> (d, false, domains)
     | LinkedNode(d, next) -> (d, false, next)) in
   match nodeAndNext with
   | (node, hasNext, next) ->
-      let matched = 
+      let matched =
         (match node with
-        | DomainId(did) -> did == id
+        | DomainId(did) -> did = id
         | DomainRange(low, high) -> id >= low && id <= high) in
-      if matched then 
-        matched
-      else
-        if hasNext then match_domain next id else false
+      matched || (hasNext && (match_domain next id))
 
-let rec checkRules (rules : rule) (sub : subject) : evalAction = 
-  match sub with
-  | Subject(_, act, id) ->
-      (match rules with
-      | head::res ->
-          (match head with 
-          | Rule(domains, isAllow, publist, sublist, relaylist) ->
-              if match_domain domains id then
-                let critList = 
-                  (match act with
-                  | SubjectAction(PUBLISH) -> publist
-                  | SubjectAction(SUBSCRIBE) -> sublist
-                  | SubjectAction(RELAY) -> relayList) in
-                if checkCriteria critList sub then 
-                  if isAllow then evalAction(ALLOW) else evalAction(DENY))
-                else checkRules res sub
-              else checkRules res sub
-      | _ -> evalAction(NONE));;
+let rec checkRules (rules : rule list) (sub : subject) : evalResult =
+  match rules with
+  | head::res ->
+    if match_domain head.domains sub.domainId then
+      let critList =
+        (match sub.action with
+        | PUBLISH -> head.publish
+        | SUBSCRIBE -> head.subscribe
+        | RELAY -> head.relay) in
+      if checkCriteria critList sub then
+        (if head.qualifier = ALLOW then ALLOWED else DENIED)
+      else checkRules res sub
+    else checkRules res sub
+  | _ -> NONE
 
-let evaluate (perm : permissions) (sub : subject) (curr_t : int) : evalAction =
-  let nodeAndNext = 
+let evaluate (perm : permissions) (sub : subject) : evalResult =
+  let nodeAndNext =
     (match perm with
     | Node(g) -> (g, false, perm)
     | LinkedNode(g, next) -> (g, true, next)) in
-  let subName = 
-    (match sub with
-    | SubJect(name, _, _) -> name) in
   match nodeAndNext with
   | (node, hasNext, next) ->
-      let matchedAndValid = 
-        (match node with
-        | grant(name, validTime, rules, default) ->
-            if name != subName then false
-            else 
-              (match validTime with
-              | Validity(low, high) -> curr_t >= low && curr_t <= high)) in
-      if matchedAndValid then 
-        let qualifier = checkRules rules sub in
-        (match qualifier with
-        | evalAction(NONE) -> default
+      let matchedAndValid =
+        if node.subject_name <> sub.subName then false
+        else sub.time >= node.validity.low && sub.time <= node.validity.high in
+      if matchedAndValid then
+        let evalRes = checkRules rules sub in
+        (match evalRes with
+        | NONE -> default
         | _ -> qualifier)
       else
-        if hasNext then evaluate next sub curr_t else evalAction(ERROR);;
-
+        if hasNext then evaluate next sub curr_t else ERROR
