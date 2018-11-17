20a0306e367e380d193a800983f9233b590be379
ruffsl <roxfoxpox@gmail.com>
2018-11-17 15:36:45 -0800

    Working fnmatch and intersect logical inference

--- verification/examples.ml
+++ verification/examples.ml
@@ -1,74 +1,71 @@
-let tmp_criteria = {
-  topics=["wold"];
-  partitions=["a"];
-  tags=[("a","b")]
-}
-let tmp_domain = DomainId(0)
-let tmp_validity = {low=0;high=2}
-let tmp_rule = {
-  domains=[tmp_domain];
-  qualifier=ALLOW;
-  publish=[tmp_criteria];
-  subscribe=[];
-  relay=[]
-}
-let tmp_grant = {
-  subject_name="tmp";
-  validity=tmp_validity;
-  rules=[tmp_rule];
-  default=DENY
-}
-let tmp_permission = [tmp_grant]
-let tmp_subject = {
-  subName = "tmp";
-  action=PUBLISH;
-  domainId=0;
-  topic="wold";
-  partition="a";
-  dataTag=("a","b");
-  time=1
-}
 
-verify(fun (x:subject) -> evaluate tmp_permission x = ALLOW)
+let perms_x =
+[{subject_name = "CN=talker"; validity = {low = 1382745600; high = 1698360330};
+  rules =
+   [{domains = [DomainRange (0, 42)]; qualifier = ALLOW;
+     publish = [{topics = ["rt/chatter/*"]; partitions = [""]; tags = []}];
+     subscribe = []; relay = []}];
+  default = DENY}]
+  ;;
 
+let perms_y =
+[{subject_name = "CN=listener";
+  validity = {low = 1382745600; high = 1698360330};
+  rules =
+   [{domains = [DomainRange (0, 42)]; qualifier = ALLOW; publish = [];
+     subscribe = [{topics = ["rt/spam"]; partitions = [""]; tags = []}];
+     relay = []}];
+  default = DENY}]
+  ;;
 
-let rec bcheckCriteria (cri : criteria list) (sub : subject) : bool =
-    match cri with
-    | [] -> false
-    | h::t ->
-        let match_criteria criteria sub =
-                inList criteria.topics sub.topic
-            && (sub.dataTag = ("", "") || inList criteria.tags sub.dataTag) in
-        match_criteria h sub || bcheckCriteria t sub
+let test (x:subject) (y:subject) bool =
+    let x_allowed = (evaluate perms_x x = ALLOW) in
+    let y_allowed = (evaluate perms_y y = ALLOW) in
 
-let rec bcheckRules (rules : rule list) (sub : subject) : qualifier =
-  match rules with
-  | [] -> NONE
-  | head::tail ->
-    let in_domain = match_domain head.domains sub.domainId in
-     let matched_criteria =
-      let critList =
-        (match sub.action with
-        | PUBLISH -> head.publish
-        | SUBSCRIBE -> head.subscribe
-        | RELAY -> head.relay) in
-     bcheckCriteria critList sub  in
-     if in_domain && matched_criteria then head.qualifier
-     else bcheckRules tail sub
+    let common_domain = (x.domainId = y.domainId) in
+    let common_topic = (x.topic = y.topic) in
+    let common_partition = (x.partition = y.partition) in
+    let common_dataTag = (x.dataTag = y.dataTag) in
+    let complementary_action = (x.action <> y.action) in
+    let qos_match = common_domain
+        && common_topic
+        && common_partition
+        && common_dataTag
+        && complementary_action in
 
-let rec bevaluate (perm : permissions) (sub : subject) : qualifier =
-  match perm with
-  | node::next ->
-      let matchedAndValid =
-        if node.subject_name <> sub.subName then false
-        else sub.time >= node.validity.low && sub.time <= node.validity.high in
-      if matchedAndValid then
-        let evalRes = bcheckRules node.rules sub in
-        (match evalRes with
-        | NONE -> node.default
-        | _  -> evalRes)
-      else
-        bevaluate next sub
-  | [] -> NONE
+    let coincide_time = (x.time = y.time) in
 
-  verify(fun (x:subject) -> evaluate tmp_permission x = bevaluate tmp_permission x)
+    x_allowed && y_allowed && coincide_time && qos_match
+;;
+instance test
+
+let perms_y =
+[{subject_name = "CN=listener";
+  validity = {low = 1382745600; high = 1698360330};
+  rules =
+   [{domains = [DomainRange (0, 42)]; qualifier = ALLOW; publish = [];
+     subscribe = [{topics = ["rt/chatter/foo"]; partitions = [""]; tags = []}];
+     relay = []}];
+  default = DENY}]
+  ;;
+
+let test (x:subject) (y:subject) bool =
+    let x_allowed = (evaluate perms_x x = ALLOW) in
+    let y_allowed = (evaluate perms_y y = ALLOW) in
+
+    let common_domain = (x.domainId = y.domainId) in
+    let common_topic = (x.topic = y.topic) in
+    let common_partition = (x.partition = y.partition) in
+    let common_dataTag = (x.dataTag = y.dataTag) in
+    let complementary_action = (x.action <> y.action) in
+    let qos_match = common_domain
+        && common_topic
+        && common_partition
+        && common_dataTag
+        && complementary_action in
+
+    let coincide_time = (x.time = y.time) in
+
+    x_allowed && y_allowed && coincide_time && qos_match
+;;
+instance test
--- verification/fnmatch.ml
+++ verification/fnmatch.ml
@@ -1,30 +1,9 @@
-let explode s =
-  let rec exp subs l =
-    if String.length subs = 0 then l
-    else
-        let elem = String.sub subs 0 1 in
-        let e = match elem with Some el -> el | None -> "" in
-        let subsub = String.sub subs 1 ((String.length subs) - 1) in
-        match subsub with
-        | Some s ->
-            let tail = exp s l in
-            e::tail
-        | None -> e::l
-    in
-      exp s []
-
-let rec _fnmatch (matchPattern:string list) (matchString:string list) : bool =
-    match (matchPattern, matchString) with
-    | (a::x, b::y) ->
-        (match a with
-        | "*" ->
-            (match x with
-            | [] -> true
-            | _ -> false)
-        | _ -> (a = b) && (_fnmatch x y))
-    | ([], []) -> true
-    | _ -> false
-
 let fnmatch (matchPattern:string) (matchString:string) : bool =
-    _fnmatch (explode matchPattern) (explode matchString)
-;;
+    if String.suffix "*" matchPattern then
+        let index = (String.length matchPattern) - 1 in
+        let prefix = (String.sub matchPattern 0 index) in
+        match prefix with
+        | Some pre -> String.prefix pre matchString
+        | None -> true
+    else
+        matchPattern = matchString
