4d39b11464dcaf42bf009de83d52dccfe069a203
babooloon <paulyang234@gmail.com>
2018-11-17 09:43:01 -0800

    finish construct

--- construct.py
+++ construct.py
@@ -1,3 +1,4 @@
+from IPython.display import display, IFrame, Image, Markdown, SVG
 import os
 import sys
 import networkx as nx
@@ -11,66 +12,85 @@ def getAllXMLFiles(path):
             files.append(os.path.join(path, filename))
     return files
 
-def parseXML(f, metadata):
+# Three assumptions are made on xml permission file.
+# 1. Default rule is DENY
+# 2. There is no deny_rule
+# 3. There is no regex
+def parseXML(f, G, topics):
     root = ET.parse(f).getroot()
     grants = root.findall("./permissions/grant")
     for grant in grants:
         subject = grant.findall("./subject_name")[0].text
-        if subject not in metadata:
-            metadata[subject] = dict()
-            # The first nested inner list is sub topic, and the second one is pub topics
-            metadata[subject]['allow_sub'] = []
-            metadata[subject]['allow_pub'] = []
-            metadata[subject]['deny_sub'] = []
-            metadata[subject]['deny_pub'] = []
-            metadata[subject]['default'] = "DENY"
+        if subject not in G:
+            G.add_node(subject)
         validity = grant.findall("./validity")[0]
         if validity and validity.findall("./not_before"):
-            metadata[subject]['not_before'] = validity.findall("./not_before")[0].text
+            G.node[subject]['not_before'] = validity.findall("./not_before")[0].text
         if validity and validity.findall("./not_after"):
-            metadata[subject]['not_after'] = validity.findall("./not_after")[0].text
+            G.node[subject]['not_after'] = validity.findall("./not_after")[0].text
         allow_rule = grant.findall("./allow_rule")
         if allow_rule:
             allows = grant.findall("./allow_rule")[0].findall("./subscribe/topics/topic")
             if allows:
                 for allow in allows:
-                    metadata[subject]['allow_sub'].append(allow.text)
+                    topic = allow.text
+                    if topic not in topics:
+                        topics.add(topic)
+                        G.add_node(topic)
+                    G.add_edge(topic, subject)
             allowp = grant.findall("./allow_rule")[0].findall("./publish/topics/topic")
             if allowp:
                 for allow in allowp:
-                    metadata[subject]['allow_pub'].append(allow.text)
-        deny_rule = grant.findall("./deny_rule")
-        if deny_rule:
-            denys = grant.findall("./deny_rule")[0].findall("./subscribe/topics/topic")
-            if denys:
-                for deny in denys:
-                    metadata[subject]['deny_sub'].append(deny.text)
-            denyp = grant.findall("./deny_rule")[0].findall("./publish/topics/topic")
-            if denyp:
-                for deny in denyp:
-                    metadata[subject]['deny_pub'].append(deny.text)
-        default = grant.findall("./default")
-        if default:
-            metadata[subject]['default'] = default[0].text
-
-def addNode(G, metadata):
+                    topic = allow.text
+                    if topic not in topics:
+                        topics.add(topic)
+                        G.add_node(topic)
+                    G.add_edge(subject, topic)
+        
+def addNode(G, metadata, topics):
+    # Add nodes of the network as vertices
     for node in metadata.keys():
-        G.add_node(node, allow_sub=str(metadata[node]['allow_sub']), allow_pub=str(metadata[node]['allow_pub']), deny_sub=str(metadata[node]['deny_sub']), deny_pub=str(metadata[node]['deny_pub']), not_before=metadata[node]['not_before'], not_after=metadata[node]['not_after'], default=metadata[node]['default'])
+        G.add_node(node, allow_sub=str(metadata[node]['allow_sub']), allow_pub=str(metadata[node]['allow_pub']), not_before=metadata[node]['not_before'], not_after=metadata[node]['not_after'], bipartite=0)
+    #Add topics of the network as vertices
+    for t in topics:
+        G.add_node(t, bipartite=1)
 
-def construct(G, metadata):
+def construct(G, metadata, topics):
+    nodes = set(G) - topics
+    # Build edges between nodes and topics
+    for node in nodes:
+        for topic in topics:
+            pass
     for n, m in itertools.permutations(G.nodes, 2):
         for perm in metadata[n]['allow_pub']:
             for p in metadata[m]['allow_sub']:
                 if p == perm:
                     G.add_edge(n, m)
 
+def plot_graph_figure(G, file_name, view='png'):
+    A = nx.nx_agraph.to_agraph(G)
+    A.add_subgraph()
+    if view is 'pdf':
+        A.draw(file_name + '.' + 'pdf', prog='dot')
+        display(IFrame(file_name + '.' + view, width=950, height=300))
+    elif view is 'png':
+        A.draw(file_name + '.' + 'png', prog='dot')
+        A.draw(file_name + '.' + 'pdf', prog='dot')
+        display(Image(file_name + '.' + view))
+    elif view is 'svg':
+        A.draw(file_name + '.' + 'svg', prog='dot')
+        display(SVG(file_name + '.' + view))
+    else:
+        raise ValueError("No view option: {}".format(view))
+
 if __name__ == '__main__':
     path = sys.argv[1]
     files = getAllXMLFiles(path)
     G = nx.MultiDiGraph()
-    metadata = dict()
+    topics = set()
     for f in files:
-        parseXML(f, metadata)
-    addNode(G, metadata)
-    construct(G, metadata)
-    nx.write_graphml(G, 'g.xml')
+        parseXML(f, G, topics)
+    #addNode(G, metadata, topics)
+    #construct(G, metadata, topics)
+    #nx.write_graphml(G, 'g.xml')
+    plot_graph_figure(G, 'G')
