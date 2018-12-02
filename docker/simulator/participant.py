787dd0331989078758b66f14bf9ee6abdb4a8d14
ruffsl <roxfoxpox@gmail.com>
2018-12-01 23:29:36 -0800

    Working relaying with filtering appends name to routing list drops message if time to live expires

--- docker/simulator/participant.py
+++ docker/simulator/participant.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import argparse
+import datetime
 import os
 import time
 import sys
 
 
-from diagnostic_msgs.msg import DiagnosticStatus
+from diagnostic_msgs.msg import DiagnosticStatus, KeyValue
 
 import rclpy
 from rclpy.node import Node
@@ -18,20 +19,43 @@ class Participant(Node):
         self.time_to_live = time_to_live
 
         super().__init__('participant')
-        self.name = self.get_namespace() + '/' + self.get_name()
-        self.publisher_ = self.create_publisher(DiagnosticStatus, '/topic')
+        self.name = os.path.join(self.get_namespace(), self.get_name())
+        self.publisher_= self.create_publisher(
+            DiagnosticStatus, '/topic')
+        self.subscription = self.create_subscription(
+            DiagnosticStatus, '/topic', self.listener_callback)
         timer_period = 1.0  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         msg = DiagnosticStatus()
         msg.level = DiagnosticStatus.OK
-        msg.name = os.path.join(self.get_namespace(), self.get_name())
+        msg.name = self.name
         msg.message = '%d' % self.count
         msg.hardware_id = '%d' % self.time_to_live
+        key_value = KeyValue(
+            key=self.name,
+            value=datetime.datetime.utcnow().isoformat())
+        msg.values = [key_value]
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.message)
+        self.get_logger().info('Publishing: %s' % msg.message)
         self.count += 1
+
+    def listener_callback(self, msg):
+        ttl = int(msg.hardware_id)
+        keys = [key_value.key for key_value in msg.values]
+        if self.name in keys:
+            return
+        else:
+            if ttl >= 0:
+                self.get_logger().info('Received: %s' % ' ,'.join(keys))
+            if ttl > 0:
+                msg.hardware_id = '%d' % (ttl - 1)
+                key_value = KeyValue(
+                    key=self.name,
+                    value=datetime.datetime.utcnow().isoformat())
+                msg.values.append(key_value)
+                self.publisher_.publish(msg)
 
 
 def main(argv=sys.argv[1:]):
