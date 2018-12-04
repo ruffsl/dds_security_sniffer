8c8abac41d557e700891367bead1a49cda51579e
ruffsl <roxfoxpox@gmail.com>
2018-12-04 00:33:14 -0800

    Add class decorator, init timeout, and example

--- docker/recon/recon.py
+++ docker/recon/recon.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import base64
+import functools
 import os
 import requests
 import signal
@@ -21,63 +22,74 @@ def base64_str_to_str(string):
     ).decode('utf-8')
 
 
+def data_decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(self, data, data_type='src', **kwargs):
+        if data_type == 'src':
+            data = {'src_base64': str_to_base64_str(data)}
+        elif data_type == 'name':
+            data = {'name': data}
+        return func(self, data, data_type, **kwargs)
+    return wrapper_decorator
+
+
 class ImandraClient:
     def __init__(self, base_url):
         self._base_url = base_url
 
     def _init(self, timout=10, cooldown=1):
-        def handler(signum, frame):
-            raise Exception("init timeout")
+        for i in range(timout):
+            try:
+                result = self._send_request('/status')
+                if result.status_code == 200:
+                    return True
+            except Exception as err:
+                time.sleep(cooldown)
+        return False
 
-        # signal.signal(signal.SIGALRM, handler)
-        # signal.alarm(timout)
-        while True:
-            result = self._send_request('/status')
+    @data_decorator
+    def _eval(self, data, data_type, as_bool=True):
+        result = self._send_request(
+            suffix='/eval/by-' + data_type,
+            data=data)
+        if as_bool:
             if result.status_code == 200:
-                break
-            time.sleep(cooldown)
-
-    def _eval(self, input, type='src'):
-        if type == 'src':
-            data = {'src_base64': str_to_base64_str(input)}
-        elif type == 'name':
-            data = {'name': input}
-        result = self._send_request(
-            suffix='/eval/by-' + type,
-            data=data)
-        rj = result.json()
-        return rj
-
-    def _verify(self, input, type='src'):
-        if type == 'src':
-            data = {'src_base64': str_to_base64_str(input)}
-        elif type == 'name':
-            data = {'name': input}
-        result = self._send_request(
-            suffix='/verify/by-' + type,
-            data=data)
-        rj = result.json()
-        # return rj
-        if rj['result'] == 'proved':
-            return True
-        elif rj['result'] == 'refuted':
-            return False
-
-    def _instance(self, input, type='src'):
-        if type == 'src':
-            data = {'src_base64': str_to_base64_str(input)}
-        elif type == 'name':
-            data = {'name': input}
-        result = self._send_request(
-            suffix='/instance/by-' + type,
-            data=data)
+                return True
+            else:
+                return False
         return result
-        rj = result.json()
-        return rj
-        if rj['result'] == 'sat':
-            return True
-        elif rj['result'] == 'unsat':
-            return False
+
+    @data_decorator
+    def _verify(self, data, data_type, as_bool=True):
+        result = self._send_request(
+            suffix='/verify/by-' + data_type,
+            data=data)
+        if as_bool:
+            rj = result.json()
+            if rj['result'] == 'proved':
+                return True
+            elif rj['result'] == 'refuted':
+                return False
+            else:
+                raise Exception("Error {}: {}".format(
+                    result.status_code, result.reason))
+        return result
+
+    @data_decorator
+    def _instance(self, data, data_type, as_bool=True):
+        result = self._send_request(
+            suffix='/instance/by-' + data_type,
+            data=data)
+        if as_bool:
+            rj = result.json()
+            if rj['result'] == 'sat':
+                return True
+            elif rj['result'] == 'unsat':
+                return False
+            else:
+                raise Exception("Error {}: {}".format(
+                    result.status_code, result.reason))
+        return result
 
     def _send_request(self, suffix, data=None):
         url = urllib.parse.urljoin(self._base_url, suffix)
@@ -112,20 +124,19 @@ class ReconAgent(ImandraClient):
         self._config()
 
     def _config(self):
-        super()._init(self)
-        self._eval(input='#redef true;;', type='src')
-        self._eval(input='#use "reachability.ml";;', type='src')
+        super()._init()
+        self._eval(data='#redef true')
+        self._eval(data='#use "reachability.ml"')
 
-    def instance_reachabile(self, perms_x, perms_y):
-        self._eval(input='#program;;', type='src')
+    def instance_reachabile(self, perms_x, perms_y, as_bool=True):
         self._eval(
-            input='let perms_x = parse_permission_file "{}";;'.format(perms_x),
-            type='src')
+            data=f'#program;; let perms_x = parse_permission_file "{perms_x}"')
         self._eval(
-            input='let perms_y = parse_permission_file {}'.format(perms_y),
-            type='src')
-        self._eval(input='#logic;;', type='src')
-        return self._instance(input='reachable', type='name')
+            data=f'#program;; let perms_y = parse_permission_file "{perms_y}"')
+        return self._instance(
+            data='reachable',
+            data_type='name',
+            as_bool=as_bool)
 
 
 def main(argv=sys.argv[1:]):
@@ -135,12 +146,29 @@ def main(argv=sys.argv[1:]):
     # args, argv = parser.parse_known_args(argv)
 
     recon_agent = ReconAgent(os.environ['IMANDIRA_URL'])
-    result = recon_agent.instance_reachabile(
+    is_sat = recon_agent._instance(
+        data='fun x -> ((x = 3) && (x = 1))',
+        data_type='src')
+    if is_sat:
+        print('woot!')
+    else:
+        print('nope!')
+
+    is_sat = recon_agent.instance_reachabile(
         perms_x='/tmp/data/talker.xml',
         perms_y='/tmp/data/listener.xml')
-    rj = result.json()
-    print(base64_str_to_str(rj['sat_example']['model']['src_base64']))
+    if is_sat:
+        print('woot!')
+    else:
+        print('nope!')
 
+    result = recon_agent.instance_reachabile(
+        perms_x='/tmp/data/talker.xml',
+        perms_y='/tmp/data/listener.xml',
+        as_bool=False)
+    rj = result.json()
+    print(rj)
+    print(base64_str_to_str(rj['sat_example']['model']['src_base64']))
 
 if __name__ == "__main__":
     main()
