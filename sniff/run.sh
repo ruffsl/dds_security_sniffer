3b2c83c56ede311e5429ee4ba2b263217061dade
plrectco <xy.ou@foxmail.com>
2018-12-04 16:45:13 -0800

    clean dir

--- sniff/run.sh
+++ sniff/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FILE=./tshark_recording.pcapng
-tshark -i br-$(docker network ls -f name=sros2_default -q) -w $FILE -F pcapng
+tshark -i br-$(docker network ls -f name=sros2_default -q) -w $FILE -F pcapng -a duration:60
 python3 extract.py ./$FILE
 python3 construct.py .
