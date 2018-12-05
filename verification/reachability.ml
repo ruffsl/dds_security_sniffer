197857738e898241eb4123c78aa203bc39766b0d
ruffsl <roxfoxpox@gmail.com>
2018-12-05 11:50:54 -0800

    Change reachable to be directional

--- verification/reachability.ml
+++ verification/reachability.ml
@@ -5,17 +5,20 @@
 let reachable (x:subject) (perms_x: permissions) (y:subject) (perms_y: permissions) bool =
     let x_allowed = (evaluate perms_x x = ALLOW) in
     let y_allowed = (evaluate perms_y y = ALLOW) in
+    let x_publisher = (x.action = PUBLISH) in
+    let y_subscriber = (y.action = SUBSCRIBE) in
 
     let common_domain = (x.domainId = y.domainId) in
     let common_topic = (x.topic = y.topic) in
     let common_partition = (x.partition = y.partition) in
     let common_dataTag = (x.dataTag = y.dataTag) in
-    let complementary_action = (x.action <> y.action) in
+    (* let complementary_action = (x.action <> y.action) in *)
     let qos_match = common_domain
         && common_topic
         && common_partition
         && common_dataTag
-        && complementary_action in
+        && x_publisher
+        && y_subscriber in
 
     let coincide_time = (x.time = y.time) in
 
