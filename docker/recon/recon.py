32f43fe0fc9710acfddc9a5fed3115b6aec16396
ruffsl <roxfoxpox@gmail.com>
2018-12-05 15:23:27 -0800

    Adjust recon script to use latest trail in data dir

--- docker/recon/recon.py
+++ docker/recon/recon.py
@@ -2,8 +2,10 @@
 import argparse
 import os
 import sys
+from pathlib import Path
 
 from construct import graph_construct
+
 from extract import extract
 
 from query import check_route
@@ -13,17 +15,21 @@ from recon_agent import ReconAgent
 
 def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', required=True)
-    parser.add_argument('--path', required=True)
+    parser.add_argument('--data_dir', required=True)
     parser.add_argument('--target', required=True)
     parser.add_argument('--source', required=True)
     args, argv = parser.parse_known_args(argv)
+    data_dir = Path(args.data_dir)
 
     recon_agent = ReconAgent(os.environ['IMANDIRA_URL'])
-    extract(fn=args.path, path=args.dir)
-    graph_construct(path=args.dir)
+
+    trial_data_dirs = [f for f in sorted(data_dir.iterdir()) if f.is_dir()]
+    latest_trial_data_dir = trial_data_dirs[-1]
+    latest_trial_data_file = list(latest_trial_data_dir.glob('*.pcapng'))[0]
+    extract(fn=str(latest_trial_data_file), path=latest_trial_data_dir)
+    graph_construct(path=latest_trial_data_dir)
     check_route(
-        path=args.dir,
+        path=latest_trial_data_dir,
         source=args.source,
         target=args.target,
         check_func=recon_agent.instance_reachabile)
